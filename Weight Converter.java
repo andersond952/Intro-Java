c70eb55bf7ebeab9000c77e225f32e34058681e7
Derek Anderson <49964267+andersond952@users.noreply.github.com>
2021-09-04 12:47:11 -0400

    Create Weight Converter.java

---
+++ Weight Converter.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Problem1;
+
+/**
+ *
+ * @author Anderson
+ */
+public class WeightConverter 
+{
+    public static void main (String[] args)
+    {
+        double a = 10;
+        double b = 50;
+        double c = 100;
+        double conversion = 0.454;
+        
+        System.out.println("10 lbs. converts to " + a*conversion + "kg");
+        System.out.println("50 lbs. converts to " + b*conversion + "kg"); 
+        System.out.println("100 lbs. converts to " + c*conversion + "kg");
+    }
+    
+}
+
+
+
+
