6304b3c3009379243bd1f0c73bc91739d2ee1c11
Derek Anderson <49964267+andersond952@users.noreply.github.com>
2021-09-04 12:47:55 -0400

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Problem2;
+
+/**
+ *
+ * @author Anderson
+ */
+public class Circle {
+    public static void main (String[] args)
+    {
+        double radius = 3.2;
+        double diameter = 0.0;
+        double perimeter = Math.PI * diameter;   
+    }   
+}
